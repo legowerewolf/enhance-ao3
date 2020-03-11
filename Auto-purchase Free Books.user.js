e9abb87d428b248f364d3c2c180dcafd074398f9
Duncan Gibson <legowerewolf@gmail.com>
2020-03-10 20:06:10 -0700

    No automatically using free audiobook points on Amazon.

--- Auto-purchase Free Books.user.js
+++ Auto-purchase Free Books.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Auto-purchase Free Books
-// @version      0.3
+// @version      0.4
 // @updateURL    https://raw.githubusercontent.com/legowerewolf/Userscripts/master/Auto-purchase%20Free%20Books.user.js
 // @downloadURL  https://raw.githubusercontent.com/legowerewolf/Userscripts/master/Auto-purchase%20Free%20Books.user.js
 // @description  Automatically purchase free ebooks from Amazon and Google Play
@@ -14,31 +14,35 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+	"use strict";
 
-    if (location.host == "play.google.com") { //Google Play Store
-        if (location.pathname.startsWith("/store/books")) { //Main book page
-            if (document.getElementsByClassName("LkLjZd ScJHi HPiPcc IfEcue  ")[document.getElementsByClassName("LkLjZd ScJHi HPiPcc IfEcue  ").length-1].textContent.includes("Free Ebook")) {
-                document.getElementsByClassName("LkLjZd ScJHi HPiPcc IfEcue  ")[document.getElementsByClassName("LkLjZd ScJHi HPiPcc IfEcue  ").length-1].click();
-            }
-        } else if (location.pathname.startsWith("/store/epurchase")) { //Modal pop-up confirmation
-            var checkReady = setInterval(function() {
-                if (document.getElementsByClassName("price") != null){
-                    if (document.getElementsByClassName("price")[0].children[0].textContent == "$0.00" && document.getElementById("loonie-purchase-ok-button") != null) {
-                        document.getElementById("loonie-purchase-ok-button").click();
-                        console.log("Clicked purchase button");
-                        clearInterval(checkReady);
-                    }
-                }
-            }, 100);
-        }
-    } else if (location.host == "www.amazon.com"){ //Amazon.com eBooks
-        if (window.location.href.includes("/dp/")) {
-            if (document.getElementsByClassName("kindle-price")[0].children[1].textContent.includes("$0.00")) {
-                document.getElementById("one-click-button").form.submit();
-            }
-        } else if (window.location.href.includes("/kindle-dbs/")) {
-            document.querySelector(".a-button-input").click();
-        }
-    }
+	if (location.host == "play.google.com") {
+		//Google Play Store
+		if (location.pathname.startsWith("/store/books")) {
+			//Main book page
+			if (document.getElementsByClassName("LkLjZd ScJHi HPiPcc IfEcue  ")[document.getElementsByClassName("LkLjZd ScJHi HPiPcc IfEcue  ").length - 1].textContent.includes("Free Ebook")) {
+				document.getElementsByClassName("LkLjZd ScJHi HPiPcc IfEcue  ")[document.getElementsByClassName("LkLjZd ScJHi HPiPcc IfEcue  ").length - 1].click();
+			}
+		} else if (location.pathname.startsWith("/store/epurchase")) {
+			//Modal pop-up confirmation
+			var checkReady = setInterval(function() {
+				if (document.getElementsByClassName("price") != null) {
+					if (document.getElementsByClassName("price")[0].children[0].textContent == "$0.00" && document.getElementById("loonie-purchase-ok-button") != null) {
+						document.getElementById("loonie-purchase-ok-button").click();
+						console.log("Clicked purchase button");
+						clearInterval(checkReady);
+					}
+				}
+			}, 100);
+		}
+	} else if (location.host == "www.amazon.com") {
+		//Amazon.com eBooks
+		if (window.location.href.includes("/dp/")) {
+			if (document.getElementsByClassName("kindle-price")[0].children[1].textContent.includes("$0.00")) {
+				document.getElementById("one-click-button").form.submit();
+			}
+		} else if (window.location.href.includes("/kindle-dbs/" && !window.location.href.includes("thankYouPage"))) {
+			document.querySelector(".a-button-input").click();
+		}
+	}
 })();

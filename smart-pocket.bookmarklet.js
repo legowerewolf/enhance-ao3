83d7994f944655876bb9380d119fbde2a56ac0f8
Duncan Gibson <legowerewolf@gmail.com>
2021-03-24 20:56:25 -0700

    Automatically close Pocket window after 5 seconds.

--- smart-pocket.bookmarklet.js
+++ smart-pocket.bookmarklet.js
@@ -3,8 +3,13 @@ javascript: (() => {
 		const URLmods = {
 			"archiveofourown.org": () => {
 				if (url.pathname.startsWith("/works/")) {
-					url.pathname = url.pathname.match(/\/works\/\d*/)[0]; /* Trim to just the work ID */
-					url.searchParams.set("view_adult", "true"); /* Bypass the adult-work interstitial */
+					url.pathname = url.pathname.match(
+						/\/works\/\d*/
+					)[0]; /* Trim to just the work ID */
+					url.searchParams.set(
+						"view_adult",
+						"true"
+					); /* Bypass the adult-work interstitial */
 					url.searchParams.set("view_full_work", "true"); /* Add all chapters */
 				}
 				return url;
@@ -15,8 +20,19 @@ javascript: (() => {
 	};
 
 	let pocketSubmitURL = new URL("https://getpocket.com/save");
-	pocketSubmitURL.searchParams.set("url", submitUrlModifier(new URL(location.href)).toString());
+	pocketSubmitURL.searchParams.set(
+		"url",
+		submitUrlModifier(new URL(location.href)).toString()
+	);
 	pocketSubmitURL.searchParams.set("title", document.title);
 
-	let w = window.open(pocketSubmitURL.toString(), "Pocket", "height=200,width=500");
+	let w = window.open(
+		pocketSubmitURL.toString(),
+		"Pocket",
+		"height=200,width=500"
+	);
+
+	setTimeout(() => {
+		w.close();
+	}, 5 * 1000);
 })();

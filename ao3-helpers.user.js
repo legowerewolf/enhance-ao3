df41857ada911f6dd030d9a78a393fd2666775b3
Duncan Gibson <legowerewolf@gmail.com>
2021-09-24 20:25:15 -0700

    Rewrite the data-parsing algorithm.

--- ao3-helpers.user.js
+++ ao3-helpers.user.js
@@ -1,50 +1,73 @@
 // ==UserScript==
-// @name         AO3 Helpers
-// @namespace    legowerewolf.net
-// @version      0.1.2
-// @updateURL    https://raw.githubusercontent.com/legowerewolf/Userscripts/master/ao3-helpers.user.js
-// @downloadURL  https://raw.githubusercontent.com/legowerewolf/Userscripts/master/ao3-helpers.user.js
-// @description  Capture work data from Archive of Our Own.
-// @author       legowerewolf.net
-// @match        https://archiveofourown.org/works/*
-// @grant        none
+// @name			AO3 Helpers
+// @namespace		legowerewolf.net
+// @version			0.2.0
+// @updateURL		https://raw.githubusercontent.com/legowerewolf/Userscripts/master/ao3-helpers.user.js
+// @downloadURL		https://raw.githubusercontent.com/legowerewolf/Userscripts/master/ao3-helpers.user.js
+// @description		Capture work data from Archive of Our Own.
+// @author			legowerewolf.net
+// @match			https://archiveofourown.org/works/*
+// @match			https://archiveofourown.org/chapters/*
+// @grant			none
 // ==/UserScript==
 
 "use strict";
 
-function main() {
-	const data = {
-		title: document.querySelector(".title.heading").innerText.trim(),
-		author: (() => {
-			let a = document.querySelector("[rel=author]");
-			return {
-				name: a.innerText.trim(),
-				profile: a.href,
-			};
-		})(),
-		tags: [
-			"rating",
-			"warning",
-			"category",
-			"fandom",
-			"relationship",
-			"character",
-			"freeform",
-		].reduce((tags, category) => {
-			return {
-				...tags,
-				[category]: Array.from(
-					page.querySelectorAll(`.${category}.tags .tag`)
-				).map((tag) => {
-					return {
-						tagName: tag.innerText,
-						tagLink: tag.href,
-					};
-				}),
-			};
-		}, {}),
+function getWorkData() {
+	let title = document.querySelector(".title.heading").innerText.trim();
+
+	let id = -1;
+	let loc = document.querySelector("li.share a").href.match(/works\/(\d+)/);
+	if (1 in loc) id = parseInt(loc[1]);
+	else console.error("Could not find work ID.");
+
+	let authorLink = document.querySelector("[rel=author]");
+	let author = {
+		name: authorLink.innerText.trim(),
+		profile: authorLink.href,
 	};
 
+	let [chapters_complete, chapters_total] = document
+		.querySelector(".stats .chapters + .chapters")
+		.innerText.split("/")
+		.map((s) => parseInt(s) || -1);
+
+	let status_text =
+		chapters_complete != chapters_total ? "In Progress" : "Complete";
+
+	let tag_categories = new Set();
+	for (let node of document.querySelectorAll(".work.meta.group .tags")) {
+		node.classList.forEach((c) => tag_categories.add(c));
+	}
+	tag_categories.delete("tags");
+
+	let tags = {};
+	for (const category of tag_categories) {
+		tags[category] = Array.from(
+			document.querySelectorAll(`.${category}.tags .tag`)
+		).map((tag) => {
+			return {
+				tagName: tag.innerText,
+				tagLink: tag.href,
+			};
+		});
+	}
+
+	return {
+		title,
+		id,
+		author,
+		tags,
+		status: {
+			text: status_text,
+			chapters_complete,
+			chapters_total,
+		},
+	};
+}
+
+function main() {
+	const data = getWorkData();
 	console.debug(data);
 }
 
